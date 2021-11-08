8ddfbb4df8430f0567ea72a17487c47f7f744456
Poga Huang <pgh86812@gmail.com>
2021-11-08 15:52:29 +0800

    commit1

--- Homework02/ViewController.swift
+++ Homework02/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
+    @IBOutlet weak var mySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBAction func changeImage(_ sender: UISegmentedControl) {
+        let imageArray = ["star_B","mustard_B","guitar_B"]
+        let index = imageArray[mySegmentedControl.selectedSegmentIndex]
+        myImageView.image = UIImage(named: index)
+        mySegmentedControl.titleForSegment(at: mySegmentedControl.selectedSegmentIndex)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mySegmentedControl.setDividerImage(UIImage(named: "line"), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+    
     }
 
 
